1ca8a011c3f49cb64ee62e9d61a9d91db82971d5
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-09-14 13:34:35 -0300

    Update ClassAgenda.py

--- AGENDA/ClassAgenda.py
+++ AGENDA/ClassAgenda.py
@@ -1,10 +1,7 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import datetime
 
 class Contato():
-    def __init__(self, pessoa, telefone):
-        self.criacao = str(datetime.date.today())
+    def __init__(self,criacao, pessoa, telefone):
+        self.criacao = criacao
         self.pessoa = pessoa
         self.telefone = telefone
 
