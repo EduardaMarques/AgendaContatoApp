ba9d1f78aa147925f9b77314efc83c8eb4fd1136
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-09-24 23:22:30 -0300

    Update ClassContato.py

--- AGENDA/ClassContato.py
+++ AGENDA/ClassContato.py
@@ -1,12 +1,9 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import datetime
 
 class Contato():
-    def __init__(self, pessoa, telefone):
+    def __init__(self, pessoa, telefones = []):
         self.criacao = str(datetime.date.today())
         self.pessoa = pessoa
-        self.telefone = telefone
+        self.telefone = telefones
 
     def listarTelefones(self):
-        pass
+        return self.telefones
