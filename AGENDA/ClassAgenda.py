77d78f303c46d5aeecc2ff3239bfae97dfca3805
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2017-09-24 23:21:38 -0300

    Update and rename ClassAgenda.py to Agenda.py

--- AGENDA/ClassAgenda.py
+++
@@ -1,9 +0,0 @@
-
-class Contato():
-    def __init__(self,criacao, pessoa, telefones = []):
-        self.criacao = criacao
-        self.pessoa = pessoa 
-        self.telefone = telefones
-
-    def listarTelefones(self):
-        return self.telefones
